9d642a3790d9781fad130ad69d827f92275accd9
IkhrazneMohamed <Mohamed.Ikhrazne@Student.HTW-Berlin.de>
2023-01-10 16:29:57 +0100

    init script

---
+++ todo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# author : mohamed ikhrazne
+# flags documentation
+# -a : print all tasks to finish
+# -t : add new task to todolist
+# -f : finish task , and move it to deletedtasks
+# -e : edit tasks
+# -n : the number of todotasks
+# -nd or -dn : the number of deleted tasks
+# --hel
+
+
+
